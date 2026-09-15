cfefdfcabcf0b90f30ad251ae5c273984a4712fb
Sumaira_Ashiq <summaashiq@gmail.com>
2026-09-15 15:51:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Mobile Navigation
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+});
+
+
+// Close mobile menu when a link is clicked
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(function (link) {
+    link.addEventListener("click", function () {
+        navLinks.classList.remove("active");
+    });
+});
+
+
+// Change navbar appearance when scrolling
+
+window.addEventListener("scroll", function () {
+
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.style.boxShadow = "0 5px 25px rgba(0, 0, 0, 0.25)";
+    } else {
+        header.style.boxShadow = "none";
+    }
+
+});
